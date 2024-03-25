4d61387d5b30d44d80ea0dba771f1010e98291a8
Felipe Sales <sales.felipeb@gmail.com>
2024-03-25 23:45:40 +0100

    Fix tests for imc function

--- test/test/imc_test.clj
+++ test/test/imc_test.clj
@@ -2,11 +2,11 @@
   (:require [clojure.test :refer :all]
             [exercises.imc :refer :all]))
 
-(deftest confere-imc
+(deftest confere-imc-testes
   (testing "IMC baixo"
     (is (= :baixo (confere-imc 1.71 30))))
   (testing "IMC alto"
     (is (= :alto (confere-imc 1.71 90))))
   (testing "IMC ideal"
-    (is (= :alto (confere-imc 1.71 60)))))
+    (is (= :ideal (confere-imc 1.71 60)))))
     
