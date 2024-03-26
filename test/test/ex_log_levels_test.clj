e5528f5545636d8a82ee63a5d04ce99f0d0314c6
Felipe Sales <sales.felipeb@gmail.com>
2024-03-26 23:57:19 +0100

    Implement log-level

--- test/test/ex_log_levels_test.clj
+++ test/test/ex_log_levels_test.clj
@@ -1,9 +1,15 @@
 (ns test.ex-log-levels-test
   (:require [clojure.test :refer :all]
             [exercises.exercism-log-levels :refer
-             [message]]))
+             [message log-level]]))
 
 (deftest check-message
   (is (= "Invalid operation" (message "[ERROR]: Invalid operation")))
   (is (= "Disk almost full" (message "[WARNING]:  Disk almost full\r\n")))
   (is (= "Timezone not set" (message "[WARNING]:   \tTimezone not set  \r\n"))))
+
+
+(deftest check-log-level
+  (is (= "error" (log-level "[ERROR]: Invalid operation")))
+  (is (= "warning" (log-level "[WARNING]:  Disk almost full\r\n")))
+  (is (= "info" (log-level "[INFO]:   \tTimezone not set  \r\n"))))
