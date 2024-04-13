c2a16d299c6f00c3709eb93df40d34c89aa25999
Felipe Sales <sales.felipeb@gmail.com>
2024-04-13 10:12:12 +0200

    Remove unnused code

--- src/exercises/exercism_card_games.clj
+++ src/exercises/exercism_card_games.clj
@@ -1,25 +1,6 @@
 (ns exercises.exercism-card-games)
 (require '[clojure.math :refer [floor]])
 
-(defn rounds
-  "Takes the current round number and returns 
-   a `list` with that round and the _next two_."
-  [n]
-  (list (+ n 0) (+ n 1) (+ n 2)))
-
-(defn concat-rounds
-  "Takes two lists and returns a single `list` 
-   consisting of all the rounds in the first `list`, 
-   followed by all the rounds in the second `list`"
-  [l1 l2]
-  (concat l1 l2))
-
-(defn contains-round?
-  "Takes a list of rounds played and a round number.
-   Returns `true` if the round is in the list, `false` if not."
-  [l n]
-  (some? (some #(= n %) l)))
-
 (defn card-average
   "Returns the average value of a hand"
   [hand]
