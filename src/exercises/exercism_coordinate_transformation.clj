4d81b47c8225ba7f6ad8d67300ecf0904d0c5112
Felipe Sales <sales.felipeb@gmail.com>
2024-04-15 08:29:41 +0200

    Implement scaled2d on transformation exercise

--- src/exercises/exercism_coordinate_transformation.clj
+++ src/exercises/exercism_coordinate_transformation.clj
@@ -1,5 +1,4 @@
-(ns exercises.exercism-coordinate-transformation
-  (:require [clojure.math :as math]))
+(ns exercises.exercism-coordinate-transformation)
 
 (defn translate2d
   "Returns a function making use of a closure to
@@ -10,7 +9,8 @@
 (defn scale2d
   "Returns a function making use of a closure to
    perform a repeatable 2d scale of a coordinate pair."
-  [sx sy])
+  [sx sy]
+  (fn [a1 a2] (conj [] (* sx a1) (* sy a2))))
 
 (defn compose-transform
   "Create a composition function that returns a function that 
--- test/test/ex_coordinate_transformation.clj
+++ test/test/ex_coordinate_transformation.clj
@@ -3,16 +3,30 @@
             [exercises.exercism-coordinate-transformation :refer 
              [translate2d scale2d compose-transform memoize-transform]]))
 
-(deftest check-approx-average
-  
-  (testing "should be reusable" 
+(deftest check-translate2d
+  (testing "should be reusable"
+    (is (let [dx 3
+              dy -5
+              translator (translate2d dx dy)
+              x2 4
+              y2 5
+              reusedExpected [7 0]]
+          (= reusedExpected (translator x2 y2))))) 
+  (testing "should be predictables"
     (is (let [dx 3 
               dy -5 
-              translator (translate2d dx dy) 
-              x2 4 
-              y2 5 
-              reusedExpected [7 0]] 
-          (= reusedExpected (translator x2 y2))))) 
-  (testing "should be predictable" 
-    (is (let [dx 3 dy -5 translator (translate2d dx dy) x1 0 y1 0 expected [3 -5]] (= expected (translator x1 y1))))))
-    
+              translator 
+              (translate2d dx dy) 
+              x1 0 y1 0 
+              expected [3 -5]] 
+          (= expected (translator x1 y1))))))
+
+(deftest check-scale2d
+  (testing "should be predictable"
+    (is (let [dx 4
+              dy 2
+              scaler (scale2d dx dy)
+              x1 1
+              y1 1
+              expected [4 2]]
+          (= expected (scaler x1 y1))))))
